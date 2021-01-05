0f54f7547def35a9ed2cd9639413f764afab6d65
Riccardo Trevisiol <riccardo.trevisiol.5@gmail.com>
2021-01-05 10:55:12 +0100

    isLocal è public

--- Stazione.h
+++ Stazione.h
@@ -14,10 +14,10 @@ public:
     int get_distance();
     virtual bool is_arriving(bool, int) = 0;
     virtual bool is_it_free(bool) = 0; //prende in input 0 se treno in andata, 1 se treno in ritorno; dice se c'è almeno un binario libero
+    bool isLocal();
 
 
 protected:
-    bool isLocal();
     Stazione(std::string, int);
     int distance_from_first;                  //distance from first; DISTANZA DALLA PRIMA STAZIONE
     std::string name;                       //nome della stazione
